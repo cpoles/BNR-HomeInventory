92962e7d03f06eb765ae5e18e93c7d2beb39ede5
cpoles <carlos.poles@griffithuni.edu.au>
2017-12-01 16:21:15 +1000

    Added ItemCell class and configured layout of prototype cell

---
+++ HomeInventory/ItemCell.swift
@@ -0,0 +1,15 @@
+//
+//  ItemCell.swift
+//  HomeInventory
+//
+//  Created by Carlos Poles on 1/12/17.
+//  Copyright © 2017 Carlos Poles. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    
+
+}
