4ad441ec3064d69440755c7310a0521afd9f6ef9
cpoles <carlos.poles@griffithuni.edu.au>
2017-12-02 18:02:51 +1000

    Connected UIControls to DetailViewControler via outlets

---
+++ HomeInventory/DetailViewController.swift
@@ -0,0 +1,46 @@
+//
+//  DetailViewController.swift
+//  HomeInventory
+//
+//  Created by Carlos Poles on 2/12/17.
+//  Copyright © 2017 Carlos Poles. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    
+    @IBOutlet var nameField: UITextField!
+    
+    @IBOutlet var serialNumberField: UITextField!
+    
+    @IBOutlet var valueField: UITextField!
+    
+    @IBOutlet var dateLabel: UILabel!
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
