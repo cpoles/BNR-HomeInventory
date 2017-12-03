89baf581e976977e9b879904e63cfda8aa1b3192
cpoles <carlos.poles@griffithuni.edu.au>
2017-12-03 16:36:21 +1000

    silver challenge3 finished.

--- HomeInventory/ItemCell.swift
+++ HomeInventory/ItemCell.swift
@@ -20,6 +20,8 @@ class ItemCell: UITableViewCell {
     // MARK:- Class Methods
     
     // This method get called on an object after it is loaded from an archive, in this case, the storyboard file
+    // This is used to update the views after changing the size of the font in Settings > General.
+    // It ensures that the the view is redraw with the updated font sizes
     override func awakeFromNib() {
         super.awakeFromNib()
         
